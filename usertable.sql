5902d575ff9eb5a2ec5a95289d9a1a0b6468ef57
Josh Roznowski <61392650+JRoz89@users.noreply.github.com>
2021-02-05 16:12:40 -0500

    Create usertable.sql

---
+++ usertable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+usersId int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+usersName varchar(128) NOT NULL,
+usersEmail varchar(128) NOT NULL,
+usersUid varchar(128) NOT NULL,
+usersPwd varchar(128) NOT NULL
+);
