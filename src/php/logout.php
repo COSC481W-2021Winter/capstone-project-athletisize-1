a5670bdf7b23d56dc15d110bfba53563eb5b4df6
Josh Roznowski <61392650+JRoz89@users.noreply.github.com>
2021-02-24 16:01:49 -0500

    Updates to log in and registration page
    Updates to log in and registration page

--- src/php/logout.php
+++ src/php/logout.php
@@ -1,8 +1,14 @@
 <?php
-
+// Initialize the session
 session_start();
-session_unset();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
 session_destroy();
-
-header("location: index.php");
-exit();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
