657eba1cce25804abccb9a04d2d62bbd69655fc2
Dylan Berquist <43017291+DylanBerquist@users.noreply.github.com>
2021-03-21 18:48:47 -0400

    Add files via upload
    Updated usertable files creates additional tables to hold profile information

--- src/includes/usertable.sql
+++ src/includes/usertable.sql
@@ -5,3 +5,18 @@ usersEmail varchar(128) NOT NULL,
 usersUid varchar(128) NOT NULL,
 usersPwd varchar(128) NOT NULL
 );
+CREATE TABLE profile (
+profileId int,
+imagePath varchar(128) NOT NULL,
+FOREIGN KEY (profileId) REFERENCES users(id)
+);
+CREATE TABLE mySports (
+mySportsId int,
+baseball int NOT NULL,
+hockey int NOT NULL,
+lacrosse int NOT NULL,
+ski int NOT NULL,
+snowboard int NOT NULL,
+soccer int NOT NULL,
+FOREIGN KEY (mySportsId) REFERENCES users(id)
+);
