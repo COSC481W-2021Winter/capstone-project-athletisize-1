41a1521ce5daff32108f184a22c5fa3a0f9390c7
Ali Ghaleb <73604031+alighaleb1@users.noreply.github.com>
2021-03-24 05:40:54 -0400

    sports page
    Style updated for sports page

--- src/php/sports.php
+++ src/php/sports.php
@@ -9,41 +9,40 @@ include_once 'header.php';
     </head>
 
     <body id="sportbody">
-        <section class="sport">
-
+        <div class="sport">
             <div class="col3">
                 <div class="snowboard">
-                    <img src="../images/sports_page/snowboard.jpg" height="300" weight="350">
+                    <img class="imgsb" src="../images/sports_page/snowboard.jpg" height="300" weight="350">
                     <a href="snowboard.php"><h2>Snowboard</h2></a>
                 </div>
                 <div class="lacrosse">
-                    <img src="../images/sports_page/lacrosse.jpg" height="300" weight="350">
+                    <img class="imgl" src="../images/sports_page/lacrosse.jpg" height="300" weight="350">
                     <a href="lacrosse.php"><h2>Lacrosse</h2></a>
                 </div>
             </div>
             
             <div class="col2">
                 <div class="ski">
-                    <img src="../images/sports_page/ski.jpg" height="300" weight="350">
+                    <img class="imgsk" src="../images/sports_page/ski.jpg" height="300" weight="350">
                     <a href="ski.php"><h2>Ski</h2></a>
                 </div>
                 <div class="soccer">
-                    <img src="../images/sports_page/soccer.jpg" height="300" weight="350">
+                    <img class="imgs" src="../images/sports_page/soccer.jpg" height="300" weight="350">
                     <a href="soccer.php"><h2>Soccer</h2></a>
                 </div>
             </div>
 
             <div class="col1">
                 <div class="baseball">
-                    <img src="../images/sports_page/baseball.jpg" height="300" weight="350"> 
+                    <img class="imgb" src="../images/sports_page/base.jpg" height="300" weight="300"> 
                     <a href="baseball_softball.php"><h2>Baseball/Softball</h2></a>
                 </div>
 
                 <div class="hockey">
-                    <img src="../images/sports_page/hockey.jpg" height="300" weight="350">
+                    <img class="imgh" src="../images/sports_page/hockey.jpg" height="300" weight="350">
                     <a href="hockey.php"><h2>Hockey</h2></a>
                 </div>
             </div>
-        </section>
+        </div>
     </body>
 </html>
