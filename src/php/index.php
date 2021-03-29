17c9c693f7264ad8a72f754fff03dc3c752bcdbf
Josh Roznowski <61392650+JRoz89@users.noreply.github.com>
2021-03-29 12:10:58 -0400

    Adding sizing page
    Adding sizing page with multi-step form and stylesheet

--- src/php/index.php
+++ src/php/index.php
@@ -21,28 +21,9 @@ include_once 'header.php';
   </div>
     
     <div>
-    <button type="button" class="modal-button" data-toggle="modal" data-target="#myModal">GET STARTED</button>
+    <button type="button" class="modal-button" onclick="document.location='sizing.php'">GET STARTED</button>
     </div>
-    <div class="modal" id="myModal">
-      <div class="modal-dialog modal-xl modal-dialog-centered">
-        <div class="modal-content">
-        
-        <div class="modal-header">
-          <h4 class="modal-title">Modal Heading</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        
-        <div class="modal-body">
-        Modal body...
-        </div>
-        
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
+    
   
   
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
