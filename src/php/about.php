dfbfea8fe92e159cec201085ca6b0e3bdb05f78d
JonathanNemchak <70524515+JonathanNemchak@users.noreply.github.com>
2021-03-28 20:42:40 -0400

    Updated About page

---
+++ src/php/about.php
@@ -0,0 +1,184 @@
+<?php
+include_once 'header.php';
+?>
+<html>
+<head>
+    <title>Sports</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link rel="stylesheet" href="https://daneden.github.io/animate.css/animate.min.css">
+    <link rel="stylesheet" href="../style/about.css">
+</head> 
+<body>
+<section>
+    <section class="team-page-section">
+        <div class="container">
+            <!-- Sec Title -->
+            <div class="sec-title centered">
+                <h1 style="color:#009999">MEET THE TEAM</h1>
+                <p style = "color: white">
+                    Meet our team here at AthletiSize! The people behind the scenes of
+                    creating a website to meet your sports equipment needs. If you would like to join our team head over to contact us and fill out the form. We are always looking for new talent. </p>
+                <hr>
+            </div>
+
+            <div class="row clearfix">
+
+                <!-- Team Block -->
+                <div class="team-block col-lg-4 col-md-6 col-sm-12">
+                    <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                        <ul class="social-icons">
+                            <li><a href="https://github.com/JRoz89"><i class="fab fa-github"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/josh-roznowski/"><i class="fab fa-linkedin-in"></i></a></li>
+                        </ul>
+                    <div class="image">
+                        <a href="#"><img src="../images/Josh.jpg" alt="" /></a>
+                    </div>
+                    <div class="lower-content">
+                        <br>
+                        <h3><a href="#">Josh</a></h3>
+                        <div class="designation">Back End Developer </div>
+                        <p style ="font-size: 12px;"> I am a back-end developer and chair for Athletisize. I love learning everything I can about coding, and challenging myself to work outside of my bubble. I love sports, especially hockey and football (Go Red Wings and Lions!), and games of all kind. I spend most of my time with my wife Amanda, and my Shepherd/Lab mix, Thanos.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Team Block -->
+            <div class="team-block col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                    <ul class="social-icons">
+                        <li><a href="https://github.com/etracht"><i class="fab fa-github"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/etracht/"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                    <div class="image">
+                        <a href="#"><img src="../images/Emily.jpg" alt="" /></a>
+                    </div>
+                    <div class="lower-content">
+                        <br>
+                        <h3><a href="#">Emily</a></h3>
+                        <div class="designation">Front Developer </div>
+                        <p style ="font-size: 12px;"> I love learning new things about artificial intelligence, deep/machine learning, statistics, math, science,  and all the other fun stuff related to computer science. I also enjoy reading, photography, sewing, and training my adorable german shepherd Hansel. Favorite quote: “Learning never exhausts the mind” - Leonardo Da Vinci.</p> 
+                    </div>
+                </div>
+            </div>
+            <!-- Team Block -->
+            <div class="team-block col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                    <ul class="social-icons">
+                      <li><a href="https://github.com/YaozeZ"><i class="fab fa-github"></i></a></li>
+                      <li><a href="https://www.linkedin.com/in/yaoze-zhang-58647120a/"><i class="fab fa-linkedin-in"></i></a></li>
+                  </ul>
+                  <div class="image">
+                    <a href="#"><img src="../images/Yaoze.jpg" alt="" /></a>
+                </div>
+                <div class="lower-content">
+                    <br>
+                    <h3><a href="#">Yaoze</a></h3>
+                    <div class="designation">Full Stack Developer </div>
+                    <p style ="font-size: 12px;"> I work on back-end and front-end of the athletisize website, I'm a computer science major at Emu, I love learning, I love programming, so I'm studying computer science right now.I am very interested in the knowledge of big data.I also like sports, like competitive sports, especially football and basketball, I think it is very interesting.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Team Block -->
+        <div class="team-block col-lg-4 col-md-6 col-sm-12">
+            <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                <ul class="social-icons">
+                 <li><a href="https://github.com/DylanBerquist"><i class="fab fa-github"></i></a></li>
+                 <li><a href="https://www.linkedin.com/in/dylan-berquist-004b841a0/"><i class="fab fa-linkedin-in"></i></a></li>
+             </ul>
+             <div class="image">
+                <a href="#"><img src="../images/Dylan.jpg" alt="" /></a>
+            </div>
+            <div class="lower-content">
+                <br>
+                <h3><a href="#">Dylan</a></h3>
+                <div class="designation"> Full Stack Developer </div>
+                <p style ="font-size: 12px;">I am a passionate computer science major at Eastern Michigan University. I love learning about all things related to technology. I also enjoy playing my bass guitar, playing chess, and spending time with friends. My favorite thing recently has been improving my physical fitness, and I am proud to say that I’ve lost over 90lbs since March 2020. </p>
+            </div>
+        </div>
+    </div>
+    <!-- Team Block -->
+    <div class="team-block col-lg-4 col-md-6 col-sm-12">
+        <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+            <ul class="social-icons">
+                <li><a href="https://github.com/JonathanNemchak"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/jonathan-nemchak-7977aa20a/"><i class="fab fa-linkedin-in"></i></a></li>
+            </ul>
+            <div class="image">
+                <a href="#"><img src="../images/Jonathan.jpg" alt="" /></a>
+            </div>
+            <div class="lower-content">
+                <br>
+                <h3><a href="#">Jonathan</a></h3>
+                <div class="designation">Front End Developer </div>
+                <p style ="font-size: 12px;"> I’m in charge of design elements. Some of my hobbies and interests include learning about war history, playing video games, and hanging out with friends. Most recent book I’ve been reading is “The art of war and other classics of eastern philosophy”.
+                    <br><br><br></p>
+            </div>
+        </div>
+    </div>
+    <!-- Team Block -->
+    <div class="team-block col-lg-4 col-md-6 col-sm-12">
+        <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+            <ul class="social-icons">
+             <li><a href="https://github.com/alighaleb1"><i class="fab fa-github"></i></a></li>
+             <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+         </ul>
+         <div class="image">
+            <a href="#"><img src="../images/Ali.jpg" alt="" /></a>
+        </div>
+        <div class="lower-content">
+            <br>
+            <h3><a href="#">Ali</a></h3>
+            <div class="designation">Front End Developer </div>
+            <p style ="font-size: 12px;"> I am the Front-back developer of this site. I enjoy playing sports , helping others, and achieving anything I set my mind to. My favorite thing to do is to learn new things everyday because everyday is a chance to learn something new. <br> <br><br></p>
+        </div>
+    </div>
+</div>
+<!-- Team Block -->
+<div class="team-block col-lg-4 col-md-6 col-sm-12">
+    <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+        <ul class="social-icons">
+         <li><a href="#"><i class="fab fa-github"></i></a></li>
+         <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+     </ul>
+     <div class="image">
+        <a href="#"><img src="../images/Rahmah.jpg" alt="" /></a>
+    </div>
+    <div class="lower-content">
+        <br>
+        <h3><a href="#">Rahmah </a></h3>
+        <div class="designation">Front End Developer </div>
+        <p style ="font-size: 12px;"> I am the Front-back developer for AthletiSize. interests, hobbies, I like learning  about Sciences, Graphic and Interior Design. I like  to spend time  with my family ,cooking and  playing tennis . "quote": Push yourself, because no one else is going to do it for you.
+            <br> <br></p>
+    </div>
+</div>
+</div>
+<!-- Team Block -->
+<div class="team-block col-lg-4 col-md-6 col-sm-12">
+    <div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+        <ul class="social-icons">
+        </ul>
+        <div class="image">
+            <a href="#"><img src="../images/Michael.png" alt="" /></a>
+        </div>
+        <div class="lower-content">
+            <br>
+            <h3><a href="#">Michael</a></h3>
+            <div class="designation">Persona</div>
+            <p style ="font-size: 12px;"> Busy single father who has a daughter playing soccer for the first time. He has never played organized sports and doesn’t have many friends who do. Knowing he has to buy equipment for his daughter, he has no resources on how to figure out what exactly he needs to buy. He wants to make sure that he is pmaking the right choice.</p>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</section>
+</section>
+</body>
+</html>
+
+<?php
+include_once 'footer.php';
+?>
